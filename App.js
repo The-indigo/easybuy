005efa2341f19db2f13401aa426e0254ef3b2209
The-indigo <adepojuadeyemi11@gmail.com>
2023-04-22 09:01:04 -0400

    screen initialization

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
+import { Ionicons } from "@expo/vector-icons";
+
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider} from "react-native-safe-area-context";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+
 import HomeScreen from './screens/HomeScreen';
+import CartScreen from './screens/CartScreen';
+import WishlistScreen from './screens/WishlistScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 
 
@@ -17,9 +23,23 @@ const AuthenticatedScreen=()=>{
   return (
     <Tab.Navigator>
       <Tab.Screen
-      
       name="Home"
       component={HomeScreen}
+      />
+
+<Tab.Screen
+      name="Cart"
+      component={CartScreen}
+      />
+
+<Tab.Screen
+      name="Wishlist"
+      component={WishlistScreen}
+      />
+
+<Tab.Screen
+      name="Profile"
+      component={ProfileScreen}
       />
 
     </Tab.Navigator>
---
+++ screens/RegisterScreen.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+
+const RegisterScreen=()=>{
+    return (
+        <View>
+            <Text>Login screen</Text>
+        </View>
+    )
+}
+export default RegisterScreen
