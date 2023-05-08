1a4aab6dd37c97db869dfac242d8e6a8723c88ed
The-indigo <adepojuadeyemi11@gmail.com>
2023-05-08 19:44:29 -0400

    cart reducer

--- store/cartReducer.js
+++ store/cartReducer.js
@@ -1,15 +1,25 @@
-const cartReducer=(state=[],action)=>{
-    switch(action.type){
-        case('INITIALIZE'):
-        return 
-        case('ADD_TO_CART'):
+import { createSlice } from "@reduxjs/toolkit"
+const initialState=[]
+const cartReducer=createSlice({
+    name:'cart',
+    initialState,
+    reducers:{
+        iniitalize(state,action){
+            return action.payload
+        },
+        addToCart(state,action){
         return [...state,action.payload]
-        case('REMOVE_FROM_CART'):
-        const id=state.find(n=>n.id===action.payload.id)
-        return state.filter(n=>n.id!==id)
-        default:
-            return state
-    } 
+        },
+        removeFromCart(state,action){
+            const id=state.find(n=>n.id===action.payload.id)
+            return state.filter(n=>n.id!==id)
+        }
+    }
+})
+
+   
+export const { iniitalize,addToCart,removeFromCart } = cartReducer.actions
+
+export default cartReducer.reducer
+
     
-}
-export default cartReducer
